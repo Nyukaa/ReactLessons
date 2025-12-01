0e666475e66e335fc252b26c7560da337f918412
Nyukaa <annashitikova@gmail.com>
2025-12-01 16:52:04 +0200

    changed package.json and server.js (work as file)

--- Lessons/server.js
+++ Lessons/server.js
@@ -1,39 +1,9 @@
-// const jsonServer = require("json-server");
-// const server = jsonServer.create();
-// const router = jsonServer.router("src/db.json");
-// const middlewares = jsonServer.defaults();
-// server.use(middlewares);
-// server.use(router);
-// server.listen(process.env.PORT || 3000, () => {
-//   console.log(" JSON Server is running");
-// });
-import express from "express";
 import jsonServer from "json-server";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const server = express();
+const server = jsonServer.create();
 const router = jsonServer.router("src/db.json");
-const middlewares = jsonServer.defaults();
-
+const middlewares = jsonServer.defaults({ static: "./dist" });
 server.use(middlewares);
-
-// serve /dist
-server.use(express.static(path.join(__dirname, "dist")));
-
-server.use("/api", router);
-
-server.listen(process.env.PORT || 3001, () => {
-  console.log("Server + API running on port 3001");
+server.use(router);
+server.listen(process.env.PORT || 3000, () => {
+  console.log(" JSON Server is running");
 });
-
-// "scripts": {
-//   "dev": "vite",
-//   "build": "vite build",
-//   "preview": "vite preview",
-//   "lint": "eslint .",
-//   "server": "npm run build && node server.js"
-// }
