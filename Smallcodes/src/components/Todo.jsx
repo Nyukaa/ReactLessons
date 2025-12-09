70fe2a2d3e11b418b01d77dc14eb798d410e38c4
Nyukaa <annashitikova@gmail.com>
2025-12-09 12:03:49 +0200

    todo

--- Smallcodes/src/components/Todo.jsx
+++ Smallcodes/src/components/Todo.jsx
@@ -2,15 +2,20 @@ import { useState } from "react";
 
 export default function CountText() {
   const products = ["Buy milk", "Walk dog", "Study React"];
-  const handleClick = (pro) => () => {
-    console.log(pro);
-  };
+
   return (
     <div>
       <h1>Products</h1>
       <ul>
         {products.map((pro) => (
-          <li key={pro} onClick={handleClick(pro)}>
+          <li
+            key={pro}
+            onClick={
+              <li key={pro} onClick={() => console.log(pro)}>
+                {pro}
+              </li>
+            }
+          >
             {pro}
           </li>
         ))}
